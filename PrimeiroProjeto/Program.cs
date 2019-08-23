d9760cce193b321cf41686cfafdca72dfc190cd2
vanercarmo@gmail.com <vaner.carmo@oletecnologia.com.br>
2019-08-23 14:55:58 -0300

    Segunda alteracao

--- PrimeiroProjeto/Program.cs
+++ PrimeiroProjeto/Program.cs
@@ -6,7 +6,7 @@ namespace PrimeiroProjeto
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Aspira!");
+            Console.WriteLine("Hello mundo!");
 
             
         }
