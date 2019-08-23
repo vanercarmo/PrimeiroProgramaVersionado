48167741f0c68f836097504e3468a0f89bbb22a1
vanercarmo@gmail.com <vaner.carmo@oletecnologia.com.br>
2019-08-23 14:48:02 -0300

    Projeto iniciado mostrando a mensagem ola mundo

---
+++ PrimeiroProjeto/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace PrimeiroProjeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Aspira!");
+
+            
+        }
+    }
+}
+
